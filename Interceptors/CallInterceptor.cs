405fa8a003cbb4f290049a7f2bc3b2c0ca695746
gr0ker <gr0ker@yandex.ru>
2024-01-23 21:22:07 +0100

    Add Interceptors

---
+++ Interceptors/CallInterceptor.cs
@@ -0,0 +1,21 @@
+﻿namespace Interceptors;
+
+using System.IO;
+using Castle.DynamicProxy;
+
+public class CallInterceptor : IInterceptor
+{
+    private readonly StreamWriter _writer;
+
+    public CallInterceptor(StreamWriter writer)
+    {
+        _writer = writer;
+    }
+
+    public void Intercept(IInvocation invocation)
+    {
+        _writer.WriteLine($"Called {invocation.TargetType.Name}.{invocation.Method.Name}");
+
+        invocation.Proceed();
+    }
+}
