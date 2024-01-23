4c5478aa06df774620af5a4a8b0562e4960eb317
gr0ker <gr0ker@yandex.ru>
2024-01-23 21:16:33 +0100

    Initial version

---
+++ MyLibrary/Subject.cs
@@ -0,0 +1,18 @@
+﻿namespace MyLibrary;
+
+using System.IO;
+
+public class Subject : ISubject
+{
+    private readonly StreamWriter _writer;
+
+    public Subject(StreamWriter writer)
+    {
+        _writer = writer;
+    }
+
+    public void DoNothing()
+    {
+        _writer.WriteLine("Nothing is done");
+    }
+}
